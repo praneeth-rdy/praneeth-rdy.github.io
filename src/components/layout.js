5b81db3cfae2fad9d21e8d56d2da6fa8897024f6
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-04-05 13:47:49 +0530

    Changed positioning in website navbar modified a div and changed its position in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,6 +46,12 @@ const Layout = props => {
               </div>
             </div>
           </a>
+          {/*to place name in center change class name to site-head-center, remove style attribute from link and place this div below nav element*/}
+          <div className="site-head-left">
+            <Link className="site-head-logo" to={`/`} style={{ opacity: 1 }}>
+              {navHeading}
+            </Link>
+          </div>
           <nav id="swup" className="site-head-left">
             <ul className="nav" role="menu">
               <li className={"nav-home " + navActive.home} role="menuitem">
@@ -65,11 +71,6 @@ const Layout = props => {
               </li>
             </ul>
           </nav>
-          <div className="site-head-center">
-            <Link className="site-head-logo" to={`/`}>
-              {navHeading}
-            </Link>
-          </div>
           <div className="site-head-right">
             <div className="social-links">
               <a
