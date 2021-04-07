67983112c9cc1fa389ab4d8c076c4ab6f41242e3
Praneeth Kolanu <k.praneeth1199@iitkgp.ac.in>
2021-04-07 23:23:46 +0530

    skillset section developed

--- src/components/homeScreen/skillsetSection.js
+++ src/components/homeScreen/skillsetSection.js
@@ -1,7 +1,8 @@
 import React from "react"
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+//import Slider from "react-slick"
+import Img from "gatsby-image"
+//import "slick-carousel/slick/slick.css"
+//import "slick-carousel/slick/slick-theme.css"
 import { StaticQuery, graphql } from "gatsby"
 import "../../styles/css/screens/homescreen/skillset-section.css"
 
@@ -17,17 +18,31 @@ const settings = {
 
 function SkillsetSection(props) {
   //use data as props.data
+  const skills = props.data.allMarkdownRemark.edges
+  let skillCounter = 0
   return (
     <section className="home-section post-content-body">
       <h2 class="main-heading">Skillset</h2>
-      <Slider {...settings} className="row">
-        <div className="card-wrapper">
-          <div className="card b-shadow grow">
-            <div className="card-image"></div>
-            <div className="card-description"></div>
-          </div>
-        </div>
-      </Slider>
+      <div className="cards-container row">
+        {skills.map(({ node }) => {
+          skillCounter++
+          return (
+            <div className="card-wrapper">
+              <div className="card b-shadow grow">
+                <h3 className="heading">{node.frontmatter.title}</h3>
+                <div className="card-image">
+                  <Img
+                    fluid={node.frontmatter.thumbnail.childImageSharp.fluid}
+                  />
+                </div>
+                <div className="card-description">
+                  <p>{node.frontmatter.description}</p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
--- src/content/skills/temp/index.md
+++ src/content/skills/temp/index.md
@@ -1,5 +1,5 @@
 ---
-title: Web Development
+title: Android Development
 date: "2019-05-08"
 thumbnail: ./img.jpg
 description: Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.
