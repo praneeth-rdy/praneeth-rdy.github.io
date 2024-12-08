854cd3aabe9e39eff9cd45c10c0d763ea90d01ff
praneeth-rdy <praneeth@trumio.ai>
2024-12-08 19:28:31 +0530

    refactor: projects

--- src/components/home/ProjectsSection.js
+++ src/components/home/ProjectsSection.js
@@ -9,7 +9,7 @@ import Tooltip from "../core/Tooltip"
 
 function ProjectsSection({ data }) {
   const [showAll, setShowAll] = useState(false)
-  const allProjects = data.allMarkdownRemark.edges
+  const allProjects = data.dataJson.projects
 
   const initialMaxCount = 2
   const visibleProjects = showAll
@@ -24,38 +24,34 @@ function ProjectsSection({ data }) {
     <section className="home-section post-content-body">
       <h2 className="section-heading">Projects</h2>
       <div className={Styles.projectCardsContainer}>
-        {visibleProjects.map(({ node }, index) => {
+        {visibleProjects.map((project, index) => {
           return (
             <div key={index} className={`${Styles.card} 'b-shadow' 'grow'`}>
               <div className={Styles.cardImage}>
                 <GatsbyImage
-                  image={
-                    node.frontmatter.thumbnail.childImageSharp.gatsbyImageData
-                  }
-                  alt={`${node.frontmatter.title} Image`}
+                  image={project.thumbnail.childImageSharp.gatsbyImageData}
+                  alt={`${project.title} Image`}
                 />
               </div>
               <div className={Styles.body}>
                 <div className={Styles.headingContainer}>
-                  <h3 className={`${Styles.heading}`}>
-                    {node.frontmatter.title}
-                  </h3>
-                  {node.frontmatter.associatedWith.logo && (
+                  <h3 className={`${Styles.heading}`}>{project.title}</h3>
+                  {project.associatedWith.logo && (
                     <Tooltip
-                      text={`This project is associated with ${node.frontmatter.associatedWith.org}`}
+                      text={`This project is associated with ${project.associatedWith.org}`}
                       position="top"
                     >
                       <img
                         src={
-                          node.frontmatter.associatedWith.logo.childImageSharp
+                          project.associatedWith.logo.childImageSharp
                             .gatsbyImageData.images.fallback.src
                         }
-                        alt={node.frontmatter.associatedWith.org}
+                        alt={project.associatedWith.org}
                         className={Styles.orgLogo}
                       />
                     </Tooltip>
                   )}
-                  {node.frontmatter.confidential && (
+                  {project.confidential && (
                     <Tooltip
                       text="Confidential information is hidden"
                       position="top"
@@ -68,11 +64,11 @@ function ProjectsSection({ data }) {
                 </div>
                 {/* <div className={Styles.datesContainer}>
                   <span className={Styles.date}>
-                    {node.frontmatter.dates.start}
+                    {project.dates.start}
                   </span>
                   <span className={Styles.dateSeparator}>-</span>
                   <span className={Styles.date}>
-                    {node.frontmatter.dates.end}
+                    {project.dates.end}
                   </span>
                 </div> */}
                 <div className={Styles.descriptionContainer}>
@@ -80,66 +76,55 @@ function ProjectsSection({ data }) {
                     className={Styles.cardDescription}
                     charLimit={300}
                   >
-                    {node.frontmatter.description}
+                    {project.description}
                   </ExpandableText>
                 </div>
                 <div className={Styles.additionalInfo}>
                   <div className={Styles.stacksContainer}>
-                    {node.frontmatter.stacks.map(
-                      ({ childImageSharp }, index) => (
-                        <GatsbyImage
-                          key={index}
-                          className={Styles.stack}
-                          image={childImageSharp.gatsbyImageData}
-                          alt={`Stack Image`}
-                          objectFit="contain"
-                        />
-                      )
-                    )}
+                    {project.stacks.map(({ childImageSharp }, index) => (
+                      <GatsbyImage
+                        key={index}
+                        className={Styles.stack}
+                        image={childImageSharp.gatsbyImageData}
+                        alt={`Stack Image`}
+                        objectFit="contain"
+                      />
+                    ))}
                   </div>
                   <div className={`icons-container ${Styles.projectLinks}`}>
-                    {node.frontmatter.attachments.map((attachment, index) => {
-                      if (attachment.repository) {
-                        return (
-                          <a
-                            key={index}
-                            className="link"
-                            href={attachment.repository}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <FaGithub />
-                          </a>
-                        )
-                      }
-                      if (attachment.documentation) {
-                        return (
-                          <a
-                            key={index}
-                            className="link"
-                            href={attachment.documentation}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <FaBookOpen />
-                          </a>
-                        )
-                      }
-                      if (attachment.site) {
-                        return (
-                          <a
-                            key={index}
-                            className="link"
-                            href={attachment.site}
-                            target="_blank"
-                            rel="noreferrer"
-                          >
-                            <FaLink />
-                          </a>
-                        )
-                      }
-                      return null
-                    })}
+                    {project.attachments.repository && (
+                      <a
+                        key={index}
+                        className="link"
+                        href={project.attachments.repository}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaGithub />
+                      </a>
+                    )}
+                    {project.attachments.documentation && (
+                      <a
+                        key={index}
+                        className="link"
+                        href={project.attachments.documentation}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaBookOpen />
+                      </a>
+                    )}
+                    {project.attachments.site && (
+                      <a
+                        key={index}
+                        className="link"
+                        href={project.attachments.site}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <FaLink />
+                      </a>
+                    )}
                   </div>
                 </div>
               </div>
@@ -162,52 +147,41 @@ function ProjectsSection({ data }) {
 }
 const indexQuery = graphql`
   query {
-    allMarkdownRemark(
-      filter: { fileAbsolutePath: { regex: "/(projects)/" } }
-      sort: { fields: [frontmatter___dates___start], order: DESC }
-    ) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
+    dataJson {
+      projects {
+        title
+        description
+        confidential
+        associatedWith {
+          org
+          logo {
+            childImageSharp {
+              gatsbyImageData(
+                width: 20
+                height: 20
+                layout: FIXED
+                transformOptions: { fit: COVER }
+              )
+            }
           }
-          frontmatter {
-            title
-            description
-            confidential
-            associatedWith {
-              org
-              logo {
-                childImageSharp {
-                  gatsbyImageData(
-                    width: 20
-                    height: 20
-                    layout: FIXED
-                    transformOptions: { fit: COVER }
-                  )
-                }
-              }
-            }
-            dates {
-              start(formatString: "MMM 'YY")
-              end(formatString: "MMM 'YY")
-            }
-            stacks {
-              childImageSharp {
-                gatsbyImageData
-              }
-            }
-            attachments {
-              repository
-              documentation
-              site
-            }
-            thumbnail {
-              childImageSharp {
-                gatsbyImageData(height: 220, width: 330)
-              }
-            }
+        }
+        dates {
+          start(formatString: "MMM 'YY")
+          end(formatString: "MMM 'YY")
+        }
+        stacks {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
+        attachments {
+          repository
+          documentation
+          site
+        }
+        thumbnail {
+          childImageSharp {
+            gatsbyImageData(height: 220, width: 330)
           }
         }
       }
